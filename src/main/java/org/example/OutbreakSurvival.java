fe320f399a608edfca1d2f2d7947c50c1d60cda3
Mark11954 <markweeting@gmail.com>
2025-03-19 15:55:19 +0100

    Naam aangepast

--- src/main/java/org/example/OutbreakSurvival.java
+++ src/main/java/org/example/OutbreakSurvival.java
@@ -6,7 +6,7 @@ import com.github.hanyaeger.api.YaegerGame;
  * Hello world!
  *
  */
-public class Tetris extends YaegerGame
+public class OutbreakSurvival extends YaegerGame
 {
     public static void main( String[] args ) {
         launch(args);
